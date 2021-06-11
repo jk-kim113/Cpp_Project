7c2a80de6b502bc4396c9be64e13ec629eb98181
JongKyeong_Kim <58102022+HNCSoo@users.noreply.github.com>
2021-06-11 17:51:00 +0900

    21.06.11 서버

--- Server_ProtoType/Server_ProtoType/SocketManager.cpp
+++ Server_ProtoType/Server_ProtoType/SocketManager.cpp
@@ -4,3 +4,17 @@ void SocketManager::AddSocket(SocketClass* socket)
 {
 	_socketGroup.push_back(socket);
 }
+
+void SocketManager::AddSocket(string classInfo, SocketClass* socket)
+{
+	_classGroup[classInfo].push_back(socket);
+}
+
+void SocketManager::SendToGroup(string classInfo)
+{
+	vector<SocketClass*>::iterator it;
+	for (it = _classGroup[classInfo].begin(); it != _classGroup[classInfo].end(); it++)
+	{
+		//send(it, )
+	}
+}
--- Server_ProtoType/Server_ProtoType/SocketManager.h
+++ Server_ProtoType/Server_ProtoType/SocketManager.h
@@ -1,12 +1,17 @@
 #pragma once
 #include <vector>
 #include "SocketClass.h"
+#include <map>
+#include <iostream>
 
 class SocketManager
 {
 private:
+	map<string, vector<SocketClass*>> _classGroup;
 	vector<SocketClass*> _socketGroup;
 
 public:
 	void AddSocket(SocketClass* socket);
+	void AddSocket(string classInfo, SocketClass* socket);
+	void SendToGroup(string classInfo);
 };
